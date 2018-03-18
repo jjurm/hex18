15cc7dce6e6d9cdea6d9cf7f20bf8c9bcb4276ef
padr31 <padr31@users.noreply.github.com>
2018-03-18 00:18:46 +0000

    Facebook add user working

--- app/src/main/java/com/treecio/hexplore/activities/ProfileActivity.kt
+++ app/src/main/java/com/treecio/hexplore/activities/ProfileActivity.kt
@@ -1,16 +1,23 @@
 package com.treecio.hexplore.activities
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import com.raizlabs.android.dbflow.data.Blob
 import com.raizlabs.android.dbflow.kotlinextensions.select
 import com.raizlabs.android.dbflow.kotlinextensions.from
 import com.raizlabs.android.dbflow.kotlinextensions.where
 import com.raizlabs.android.dbflow.kotlinextensions.eq
+import com.squareup.picasso.Picasso
 import com.treecio.hexplore.R
 import com.treecio.hexplore.model.User
 import com.treecio.hexplore.model.User_Table
+import android.content.Intent
+import android.net.Uri
+
 
 const val USER_ID = "com.treecio.hexplore.MESSAGE"
 
@@ -34,6 +41,28 @@ class ProfileActivity : AppCompatActivity() {
         }
         findViewById<TextView>(R.id.txt_profile_shake_count).apply {
             text = user.handshakeCount.toString()
+        }
+
+        val imgProfile = findViewById<ImageView>(R.id.img_profile)
+        Picasso.get().load(user.profilePhoto).into(imgProfile)
+
+        findViewById<Button>(R.id.btn_fb_add).setOnClickListener({
+            _ -> this.startActivity(getOpenFacebookIntent(user?.profileUrl, this))
+        })
+
+    }
+
+    fun getOpenFacebookIntent(url:String?, context: Context): Intent {
+
+        try {
+            //Trys to make intent with FB's URI
+            context.getPackageManager()
+                    .getPackageInfo("com.facebook.katana", 0) //Checks if FB is even installed.
+            return Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://facewebmodal/f?href=" + url))
+        } catch (e: Exception) {
+            return Intent(Intent.ACTION_VIEW,
+                    Uri.parse(url)) //catches and opens a url to the desired page
         }
 
     }
--- app/src/main/java/com/treecio/hexplore/model/UserAdapter.kt
+++ app/src/main/java/com/treecio/hexplore/model/UserAdapter.kt
@@ -5,8 +5,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.raizlabs.android.dbflow.list.FlowQueryList
+import com.squareup.picasso.Picasso
 import com.treecio.hexplore.R
 import com.treecio.hexplore.activities.ProfileActivity
 
@@ -16,9 +18,12 @@ class UserAdapter(private val userList:FlowQueryList<User>): RecyclerView.Adapte
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
 
-        holder?.txtName?.text = userList[position]?.name ?: "Unknown"
-        holder?.txtShakeCount?.text = userList[position]?.handshakeCount.toString()
+
+        val user = userList[position]
+        holder?.txtName?.text = user?.name ?: "Unknown"
+        holder?.txtShakeCount?.text = user?.handshakeCount.toString()
         holder?.txtDescription?.text = "No description yet"
+        Picasso.get().load(user?.profilePhoto).into(holder?.imgProfile)
 
 
         holder?.itemView?.setOnClickListener{
@@ -51,7 +56,7 @@ class UserAdapter(private val userList:FlowQueryList<User>): RecyclerView.Adapte
         val txtName = itemView.findViewById<TextView>(R.id.person_card_name)
         val txtShakeCount = itemView.findViewById<TextView>(R.id.person_card_shake_count)
         val txtDescription = itemView.findViewById<TextView>(R.id.person_card_description)
-
+        val imgProfile = itemView.findViewById<ImageView>(R.id.person_card_img)
     }
 
 }
